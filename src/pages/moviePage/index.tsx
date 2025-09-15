e12af2503371ab6852ab92952ced981195f05adb
Snorre Aldstedt <snorre.alstedt@experisacademy.no>
2025-09-15 08:39:31 +0200

    Scroll to top when switching page

--- src/pages/moviePage/index.tsx
+++ src/pages/moviePage/index.tsx
@@ -1,5 +1,5 @@
 import { ContentContext } from "../../App"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { useLocation } from "react-router-dom";
 import './style.css'
 
@@ -13,11 +13,15 @@ function Moviepage(){
     const imagePoster = imageLink ? imageLink+"?location=moviePoster" : "none"
 
     const {movieFeedContent} = useContext(ContentContext);
+
+    useEffect(()=> {
+        window.scrollTo(0,0)
+    }, [])
+
     return (
         <section className="main" style= {{
             background: `linear-gradient(to bottom, rgba(26,0,51,0.5) 0%, #110021ff 70%), url(${imageLink+"?width=1920"})`,
             backgroundSize: "contain"}}>
-            {/*<img className="movie-image" src={imageLink+"?width=1920"} alt={movie.title}/>*/}
             <div className="info-movie-div">
                 <div className="text-info">
                     <h2>{movie?.title ? movie.title : "Loading movie"}</h2>
